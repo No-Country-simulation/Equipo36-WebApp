ffe1aa1e2c7eb72151c54b9d572877632ff1f9d6
EdgarCamberos1894 <edgarcamberos18@gmail.com>
2025-10-02 19:03:29 -0700

    Initial Commit Backend

---
+++ backend/VitalMedic/src/test/java/com/vitalmedic/VitalMedic/VitalMedicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalmedic.VitalMedic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalMedicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
